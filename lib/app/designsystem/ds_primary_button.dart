1eaf68c6a06559006866e23104af353f812057db
mduccc <mducc1412@gmail.com>
2022-03-18 00:06:51 +0700

    Enhancement DSPrimaryButton

--- lib/app/designsystem/ds_primary_button.dart
+++ lib/app/designsystem/ds_primary_button.dart
@@ -5,22 +5,31 @@ import 'package:flutter/material.dart';
 class DSPrimaryButton extends StatelessWidget {
   final String title;
   final GestureTapCallback onTap;
+  final bool enable;
   const DSPrimaryButton({
     required this.title,
     required this.onTap,
+    this.enable = true,
     Key? key,
   }) : super(key: key);
+
+  LinearGradient get _resolveBackgroudColor => enable
+      ? const LinearGradient(
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+          colors: [DSColors.tulipTree, DSColors.yellowOrange])
+      : const LinearGradient(
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+          colors: [DSColors.silver2, DSColors.silver2]);
 
   @override
   Widget build(BuildContext context) => GestureDetector(
         onTap: onTap,
         child: Container(
-          decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(5)),
-              gradient: LinearGradient(
-                  begin: Alignment.centerLeft,
-                  end: Alignment.centerRight,
-                  colors: [DSColors.tulipTree, DSColors.yellowOrange])),
+          decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(Radius.circular(5)),
+              gradient: _resolveBackgroudColor),
           width: double.infinity,
           height: 50,
           child: Align(
