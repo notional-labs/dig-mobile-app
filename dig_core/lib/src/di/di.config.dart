7114c449545434ddd86ca0e5abd209c16413af3d
mduccc <mducc1412@gmail.com>
2022-03-21 22:32:54 +0700

    Enhancement for cosmos auth feature

--- dig_core/lib/src/di/di.config.dart
+++ dig_core/lib/src/di/di.config.dart
@@ -7,14 +7,23 @@
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
-import '../data/datasource/local/create_db_datasource.dart'
-    as _i3; // ignore_for_file: unnecessary_lambdas
+import '../data/datasource/local/create_db_datasource.dart' as _i5;
+import '../data/repository/auth_repository_impl.dart' as _i4;
+import '../domain/repository/auth_repository.dart' as _i3;
+import '../domain/usecase/create_mnemonic_usecase.dart' as _i6;
+import '../domain/usecase/import_account_usecase.dart'
+    as _i7; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
 _i1.GetIt $initGetIt(_i1.GetIt get,
     {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
   final gh = _i2.GetItHelper(get, environment, environmentFilter);
-  gh.factory<_i3.CreateDBDatasource>(() => _i3.CreateDBDatasourceImpl());
+  gh.factory<_i3.AuthRepository>(() => _i4.AuthRepisitoryImplement());
+  gh.factory<_i5.CreateDBDatasource>(() => _i5.CreateDBDatasourceImpl());
+  gh.factory<_i6.CreateMnemonicUseCase>(
+      () => _i6.CreateMnemonicUseCase(get<_i3.AuthRepository>()));
+  gh.factory<_i7.ImportAccountUseCase>(
+      () => _i7.ImportAccountUseCase(get<_i3.AuthRepository>()));
   return get;
 }
