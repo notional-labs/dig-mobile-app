8977f4bd9895957f2e1b5e764ab186aa3df0aba5
mduccc <mducc1412@gmail.com>
2022-03-23 12:06:22 +0700

    Add CheckMnemonicValidUseCase

--- dig_core/lib/src/di/di.config.dart
+++ dig_core/lib/src/di/di.config.dart
@@ -7,12 +7,13 @@
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
-import '../data/datasource/local/create_db_datasource.dart' as _i5;
+import '../data/datasource/local/create_db_datasource.dart' as _i6;
 import '../data/repository/auth_repository_impl.dart' as _i4;
 import '../domain/repository/auth_repository.dart' as _i3;
-import '../domain/usecase/create_mnemonic_usecase.dart' as _i6;
+import '../domain/usecase/check_mnemonic_valid_usecase.dart' as _i5;
+import '../domain/usecase/create_mnemonic_usecase.dart' as _i7;
 import '../domain/usecase/import_account_usecase.dart'
-    as _i7; // ignore_for_file: unnecessary_lambdas
+    as _i8; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -20,10 +21,12 @@ _i1.GetIt $initGetIt(_i1.GetIt get,
     {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
   final gh = _i2.GetItHelper(get, environment, environmentFilter);
   gh.factory<_i3.AuthRepository>(() => _i4.AuthRepositoryImplement());
-  gh.factory<_i5.CreateDBDatasource>(() => _i5.CreateDBDatasourceImpl());
-  gh.factory<_i6.CreateMnemonicUseCase>(
-      () => _i6.CreateMnemonicUseCase(get<_i3.AuthRepository>()));
-  gh.factory<_i7.ImportAccountUseCase>(
-      () => _i7.ImportAccountUseCase(get<_i3.AuthRepository>()));
+  gh.factory<_i5.CheckMnemonicValidUseCase>(
+      () => _i5.CheckMnemonicValidUseCase());
+  gh.factory<_i6.CreateDBDatasource>(() => _i6.CreateDBDatasourceImpl());
+  gh.factory<_i7.CreateMnemonicUseCase>(
+      () => _i7.CreateMnemonicUseCase(get<_i3.AuthRepository>()));
+  gh.factory<_i8.ImportAccountUseCase>(
+      () => _i8.ImportAccountUseCase(get<_i3.AuthRepository>()));
   return get;
 }
