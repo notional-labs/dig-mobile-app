da4cae194146d28cc17070ce0e32890aed7229f9
DucNM <manhducnguyen.it@gmail.com>
2022-03-24 23:53:00 +0700

    Check if app has an account in splash screen

--- lib/app/cubit/splash/splash_cubit.dart
+++ lib/app/cubit/splash/splash_cubit.dart
@@ -1,18 +1,30 @@
+import 'package:dig_core/dig_core.dart';
 import 'package:dig_mobile_app/app/cubit/splash/splash_state.dart';
 import 'package:dig_mobile_app/app/definition/string.dart';
 import 'package:dig_mobile_app/app/route/dig_route.dart';
+import 'package:dig_mobile_app/di/di.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
 
 @injectable
 class SplashCubit extends Cubit<SplashState> {
+  final GetSelectedAccountUseCase _getAccountListUseCase = di();
+  final ChainENV _chain = di<ENV>().digChain;
+
   SplashCubit() : super(const SplashUninitState());
 
-  /// TODO: Impl [checkAuthencation]
-  void checkAuthencation() {
-    Future.delayed(const Duration(seconds: 2), () {
-      navigatorKey.currentState
-          ?.pushNamedAndRemoveUntil(DigPageName.signIn, (route) => false);
+  void checkAuthentication() {
+    Future.delayed(const Duration(seconds: 2), () async {
+      final result = await _getAccountListUseCase
+          .call(GetSelectedAccountUseCaseParam(chain: _chain));
+      result.fold((failure) {
+        navigatorKey.currentState
+            ?.pushNamedAndRemoveUntil(DigPageName.signIn, (route) => false);
+      }, (account) {
+        navigatorKey.currentState?.pushNamedAndRemoveUntil(
+            DigPageName.home, (route) => false,
+            arguments: account);
+      });
     });
   }
 }
