7ace5dccb7c8d0b3502d72a6c174198514097d28
mduccc <mducc1412@gmail.com>
2022-04-09 18:49:55 +0700

    Enhancement handle error network

--- dig_core/lib/src/data/exception/dig_exception_impl.dart
+++ dig_core/lib/src/data/exception/dig_exception_impl.dart
@@ -1,8 +1,13 @@
+import 'package:dig_core/src/domain/entity/error_response.dart';
 import 'package:dig_core/src/domain/exception/dig_exception.dart';
 
 class DigServerException extends BaseDigException {
-  final int code;
-  const DigServerException({this.code = -1, String message = ''})
+  final int statusCode;
+  final ErrorResponse errorResponse;
+  const DigServerException(
+      {this.statusCode = -1,
+      String message = '',
+      this.errorResponse = const ErrorResponse()})
       : super(message: message);
 
   BaseDigException copyWith({
@@ -10,21 +15,20 @@ class DigServerException extends BaseDigException {
     String? message,
   }) {
     return DigServerException(
-      code: code ?? this.code,
+      statusCode: code ?? this.statusCode,
       message: message ?? this.message,
     );
   }
 
   @override
-  List<Object?> get props => [code, message];
+  List<Object?> get props => [statusCode, message];
 
   @override
   bool? get stringify => true;
 }
 
 class DigNetworkException extends BaseDigException {
-  const DigNetworkException({String message = ''})
-      : super(message: message);
+  const DigNetworkException({String message = ''}) : super(message: message);
 
   @override
   List<Object?> get props => const [];
