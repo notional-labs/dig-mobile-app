6cf2cd8abe0931b12866aa6c3cbaa626ce25ae3b
mduccc <mducc1412@gmail.com>
2022-04-09 22:51:16 +0700

    Fix hardcode

--- dig_core/lib/src/domain/usecase/account/get_selected_account_usecase.dart
+++ dig_core/lib/src/domain/usecase/account/get_selected_account_usecase.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:dig_core/dig_core.dart';
+import 'package:dig_core/src/domain/definition/definition.dart';
 import 'package:dig_core/src/domain/repository/auth_repository.dart';
 import 'package:dig_core/src/domain/usecase/usecase.dart';
 import 'package:injectable/injectable.dart';
@@ -23,7 +24,7 @@ class GetSelectedAccountUseCase
       final accountIdSelected = await _repository.getLastSelectedAccountId();
 
       if (accounts.isEmpty) {
-        throw Exception('No account found');
+        throw Exception(DomainErrorMessage.noAccountFound);
       }
 
       final account = accounts.firstWhereOrNull(
