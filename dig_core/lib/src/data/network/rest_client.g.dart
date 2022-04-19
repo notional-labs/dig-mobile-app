f1ee114ad80a32d94daa3b9f388a8a3355aa31e3
DucNM <manhducnguyen.it@gmail.com>
2022-04-19 16:50:33 +0700

    Fetch proposer domain + data layer

--- dig_core/lib/src/data/network/rest_client.g.dart
+++ dig_core/lib/src/data/network/rest_client.g.dart
@@ -57,6 +57,22 @@ class _RestClient implements RestClient {
     return value;
   }
 
+  @override
+  Future<ProposerResponse> getProposer({required proposerId}) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final _result = await _dio.fetch<Map<String, dynamic>>(
+        _setStreamType<ProposerResponse>(
+            Options(method: 'GET', headers: _headers, extra: _extra)
+                .compose(_dio.options, 'gov/proposals/${proposerId}/proposer',
+                    queryParameters: queryParameters, data: _data)
+                .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = ProposerResponse.fromJson(_result.data!);
+    return value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
