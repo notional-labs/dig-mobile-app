9262522afc5f14cba2227fe5a2c1d5c9fe802c85
DucNM <manhducnguyen.it@gmail.com>
2022-05-28 11:11:16 +0700

    Implement get delegator rewards domain layer

--- dig_core/lib/src/domain/repository/delegation_repository.dart
+++ dig_core/lib/src/domain/repository/delegation_repository.dart
@@ -1,9 +1,14 @@
 import 'package:dig_core/src/domain/entity/staking/delegations/delegations_response.dart';
+import 'package:dig_core/src/domain/entity/staking/delegations/delegator_rewards_response.dart';
 import 'package:dig_core/src/domain/entity/staking/delegations/requests/get_delegations_request.dart';
+import 'package:dig_core/src/domain/entity/staking/delegations/requests/get_delegator_rewards_request.dart';
 import 'package:dig_core/src/domain/env/env.dart';
 
 abstract class DelegationRepository {
   ChainENV createChainENV(ChainENV chain);
 
   Future<DelegationsResponse> getDelegations(GetDelegationsRequest request);
+
+  Future<DelegatorRewardsResponse> getDelegatorRewards(
+      GetDelegatorRewardsRequest request);
 }
